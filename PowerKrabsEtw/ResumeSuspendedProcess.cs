6aa51f27272f183cdc2be97a496b3245657f2234
Zac Brown <zacbrown@users.noreply.github.com>
2017-10-21 19:35:48 -0700

    Add Resume-SuspendedProcess
    Signed-off-by: Zac Brown <zacbrown@users.noreply.github.com>

---
+++ PowerKrabsEtw/ResumeSuspendedProcess.cs
@@ -0,0 +1,18 @@
+﻿using PowerKrabsEtw.Internal;
+using System.Management.Automation;
+
+namespace PowerKrabsEtw
+{
+    [Cmdlet(VerbsLifecycle.Resume, "SuspendedProcess")]
+    public class ResumeSuspendedProcess : PSCmdlet
+    {
+        [Parameter(Mandatory = true, Position = 0)]
+        [ValidateNotNull]
+        public PSProcessObject ProcessObject { get; set; }
+
+        protected override void BeginProcessing()
+        {
+            NativeMethods.ResumeProcess(ProcessObject.ThreadHandle);
+        }
+    }
+}
